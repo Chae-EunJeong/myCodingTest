c66e83f584dc5f63443de7a97bc9464103c75434
HyunJoon Jeong <chaeun.jjj@gmail.com>
2020-08-05 01:02:52 +0900

    solving new problem

---
+++ StringToInt/StringToInt.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringToInt {
+    public static int solution(String s) {
+        int answer = 0;
+        
+        if (s.charAt(0) == '+') {
+
+        }
+        else if (s.charAt(0) != '-') {
+
+        }
+        else {
+
+        }
+        
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner get = new Scanner(System.in);
+        String s = get.nextLine();
+
+        int ans = solution(s);
+        System.out.println(ans);
+    }
+}
