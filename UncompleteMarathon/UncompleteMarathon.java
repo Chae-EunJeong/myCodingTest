dcad010f61f130993ef4043788e20565a8a8b652
Chae-EunJeong <68576770+Chae-EunJeong@users.noreply.github.com>
2020-08-06 18:16:06 +0900

    Update UncompleteMarathon.java

--- UncompleteMarathon/UncompleteMarathon.java
+++ UncompleteMarathon/UncompleteMarathon.java
@@ -1,5 +1,23 @@
-public class UncompleteMarathon {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> hm = new HashMap<> ();
+        for(String par : participant)
+            hm.put(par, hm.getOrDefault(par, 0) + 1);
+        
+        for(String com : completion)
+            hm.put(com, hm.get(com) - 1);
+        
+        for(String key : hm.keySet())
+            if(hm.get(key) != 0)
+                answer = key;
+        
+        return answer;
     }
 }
