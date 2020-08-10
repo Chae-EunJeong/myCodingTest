539c13cc919482262743738008cce8758c4c4d86
HyunJoon Jeong <chaeun.jjj@gmail.com>
2020-08-10 23:42:35 +0900

    complete coding

--- StringToInt/StringToInt.java
+++ StringToInt/StringToInt.java
@@ -4,21 +4,29 @@ public class StringToInt {
     public static int solution(String s) {
         int answer = 0;
         
-        for(int i = 0; i < s.length(); i++) {
-            if (s.charAt(0) == '+') {
-                
-            }else if (s.charAt(0) != '-') {
-
-            }else {
-
+        /* condition whether the first character of the string is
+        plus sign(be able to omit) or minus sign */
+        /* multiply with 10 power of the order of magnitude */
+        if (s.charAt(0) == '+') {       // if the first character is plus sign
+            for(int i = 1; i < s.length(); i++) {
+                answer += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
             }
+        }else if (s.charAt(0) != '-') { // else if the first character is minus sign
+            for(int i = 0; i < s.length(); i++) {
+                answer += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
+            }
+        }else {                         // else if the first character is the number(omitted sign)
+            for(int i = 1; i < s.length(); i++) {
+                answer += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
+            }
+            answer *= (-1);
         }
-        
-        
+
         return answer;
     }
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
+        System.out.print("input << ");
         String s = get.nextLine();
 
         int ans = solution(s);
