227787003d81f2e04171beb373906d0222c57a4d
HyunJoon Jeong <chaeun.jjj@gmail.com>
2020-08-11 00:18:26 +0900

    complete StringToInt

--- StringToInt/StringToInt.java
+++ StringToInt/StringToInt.java
@@ -2,27 +2,27 @@ import java.util.Scanner;
 
 public class StringToInt {
     public static int solution(String s) {
-        int answer = 0;
+        int number = 0;
         
         /* condition whether the first character of the string is
         plus sign(be able to omit) or minus sign */
         /* multiply with 10 power of the order of magnitude */
         if (s.charAt(0) == '+') {       // if the first character is plus sign
             for(int i = 1; i < s.length(); i++) {
-                answer += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
+                number += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
             }
         }else if (s.charAt(0) != '-') { // else if the first character is minus sign
             for(int i = 0; i < s.length(); i++) {
-                answer += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
+                number += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
             }
         }else {                         // else if the first character is the number(omitted sign)
             for(int i = 1; i < s.length(); i++) {
-                answer += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
+                number += (Character.getNumericValue(s.charAt(i)) * Math.pow(10, s.length() - (i+1)));
             }
-            answer *= (-1);
+            number *= (-1);
         }
 
-        return answer;
+        return number;
     }
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
