248a83878505a09a3a28ad4d7ea97090c79d373a
Chae-EunJeong <chaeun.jjj@gmail.com>
2020-09-05 18:47:08 +0900

    find kim

---
+++ programmers/level01/FindKimInSeoul/FindKimInSeoul.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class FindKimInSeoul {
+    public String findKimInSeoul(String[] seoul) {
+        String answer = "";
+        int where = 0;
+        ArrayList<String> seoulToList = new ArrayList<>(Arrays.asList(seoul));
+        where = seoulToList.indexOf("Kim");
+        answer = "김서방은 " + where + "에 있다";
+        return answer;
+    }
+    public static void main(String[] args) {
+        FindKimInSeoul findKim = new FindKimInSeoul();
+        String[] test = {"Jane", "Kim"};
+        
+    }
+}
