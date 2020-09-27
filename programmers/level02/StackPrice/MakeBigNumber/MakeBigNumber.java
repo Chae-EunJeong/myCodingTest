e6fcb6967c9271f3c31f32ffe76c2851ea9a410b
HyunJoon Jeong <chaeun.jjj@gmail.com>
2020-09-27 23:55:42 +0900

    new

---
+++ programmers/level02/StackPrice/MakeBigNumber/MakeBigNumber.java
@@ -0,0 +1,17 @@
+/*
+- 요구사항 : 주어진 배열의 숫자들을 이어붙여 만들 수 있는 가장 큰 수를 구해라
+- 함수
+    input : int[] 배열
+    restriction : input 배열의 길이는 1이상 100,000 이하
+                input 배열의 원소는 0이상 1000이하
+                정답이 너무 커질 수 있기 때문에 문자열로 변환해서 반환
+    output : String 문자열
+- 손코딩
+    
+*/
+
+package programmers.level02.StackPrice.MakeBigNumber;
+
+public class MakeBigNumber {
+    
+}
