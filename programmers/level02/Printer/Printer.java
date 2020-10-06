6e8b6f776ab2d776cbb7dd4598eb7d6f309773b5
HyunJoon Jeong <chaeun.jjj@gmail.com>
2020-10-06 23:58:56 +0900

    printer complete

--- programmers/level02/Printer/Printer.java
+++ programmers/level02/Printer/Printer.java
@@ -9,17 +9,75 @@
                 문서의 위치는 0이상, 대기목록의 문서의 수 -1 이하
     output : 인쇄를 요청한 문서가 인쇄될 순서(int)
 - 손코딩
+    list의 0번째보다 큰 요소가 있는지 검사
+    있으면
+        list에 0번째 요소를 새로 추가 후 0번재 요소 제거
+        location이 0이면 
+            location += list의 길이
+        location이 0이 아니면 
+            location -= 1
+    없으면
+        list에서 제거
+        인쇄수(answer + 1)
+        location이 0이면
+            return 인쇄수(answer)
+        location이 0이 아니면
+            location -= 1
+- 다른 풀이
+
     
 */
 
 package programmers.level02.Printer;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Printer {
     public int printer(int[] priorities, int location) {
+        int answer = 0;
+        boolean check = false;
+        ArrayList<Integer> priorityList = new ArrayList<>();
         
+        for(int i = 0; i < priorities.length; i++)
+            priorityList.add(priorities[i]);
+        
+        for(int j = 0; j < priorityList.size(); ){
+            check = false;
+            for (int k = priorityList.size()-1; k > 0; k--){
+                if(priorityList.get(k) > priorityList.get(j)){
+                    check = true;
+                    if(j == location)
+                        location = priorityList.size() -1;
+                    else
+                        location -= 1;
+                    priorityList.add(priorityList.get(j));
+                    priorityList.remove(j);
+  
+                    break;
+                }
+            }
+            if(!check){
+                priorityList.remove(j);
+                answer += 1;
+                if(j == location)
+                    return answer;
+                else
+                    location -= 1;
+            }          
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
-        
+        Printer print = new Printer();
+        int[] test1priorities = {2, 1, 3, 2};
+        int test1location = 2;
+        int[] test2priorities = {1, 1, 9, 1, 1, 1};
+        int test2location = 0;
+        int[] test3priorities = {1, 2, 3};
+        int test3location = 3;
+        System.out.println(print.printer(test1priorities, test1location));
+        System.out.println(print.printer(test2priorities, test2location));
+        System.out.println(print.printer(test3priorities, test3location));
     }
 }
