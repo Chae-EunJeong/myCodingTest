275c0edeb3c44f833127dc4175950f1f5f64f189
HyunJoon Jeong <chaeun.jjj@gmail.com>
2020-10-10 23:15:28 +0900

    start target number

---
+++ programmers/level02/TargetNumber/TargetNumber.java
@@ -0,0 +1,24 @@
+/*
+- 요구사항 : 입력된 자연수로 이루어진 배열의 숫자들을 적절히 빼고 더해서, 
+            타겟 넘버를 만들 수 있는 방법의 수를 반환해라.
+- 함수
+    input : 사용할 수 있는 숫자가 담긴 int 배열, 타겟 넘버 int
+    restriction : 입력된 정수형 배열의 원소의 개수는 2개 이상 20개 이하
+                  각 원소는 1이상 50이하인 자연수
+                  타겟 넘버는 1이상 1000이하인 자연수
+    output : 타겟 넘버를 만드는 방법의 수 int
+- 손코딩
+    
+- 다른 풀이
+    - 
+*/
+
+public class TargetNumber {
+    public int targetNumber(int[] numbers, int target) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
