6780e26e166efbdbc2fa9e90de7b4b188e128603
HyunJoon Jeong <chaeun.jjj@gmail.com>
2020-11-15 21:30:56 +0900

    first level3 challenge

---
+++ programmers/level03/Network/Network.java
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+public class Network {
+    public int network(int n, int[][] computers) {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Network n1 = new Network();
+        
+    }
+}
