26e0c334ca2b5725334518d2e7ac62abfd1d4bc1
HyunJoon Jeong <chaeun.jjj@gmail.com>
2020-12-02 00:01:15 +0900

    network

--- programmers/level03/Network/Network.java
+++ programmers/level03/Network/Network.java
@@ -2,7 +2,13 @@
 - 요구사항 : 컴퓨터들의 연결 정보를 가지고 존재하는 네트워크의 개수를 반환해라.
 - 함수
     - input : 컴퓨터의 개수(int n), 연결에 대한 정보(int[][] computers)
-    - constraints : 1 <
+    - constraints : 1 <= n <= 200
+                    각 컴퓨터의 순번은 0부터 n-1로 표현
+                    i번 컴퓨터와 j번 컴퓨터가 연결되어 있으면 computer[i][j] = 1
+                    자기 자신에 대한 연결정보는 항상 1 (computers[i][j] = 1)
+    - output : 네트워크의 개수 (int)
+- 손코딩
+
 */
 
 public class Network {
