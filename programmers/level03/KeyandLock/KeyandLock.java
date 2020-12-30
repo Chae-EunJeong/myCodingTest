d96d84f3138c07c291af3ac5960711638bdd5631
Chae-EunJeong <chaeun.jjj@gmail.com>
2020-12-30 18:16:00 +0900

    turned key

--- programmers/level03/KeyandLock/KeyandLock.java
+++ programmers/level03/KeyandLock/KeyandLock.java
@@ -1,3 +1,35 @@
+package programmers.level03.KeyandLock;
+
 public class KeyandLock {
+    public int[][] rightTurnKey(int[][] key) {
+        int len = key.length;
+        int[][] turnedKey = new int[len][len];
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < len; j++) {
+                turnedKey[j][len-1-i] = key[i][j];
+            }
+        }
+
+        return turnedKey;
+    }
+
+    public void printTwoDimensionalArray(int[][] tdarr) {
+        for(int i = 0; i < tdarr.length; i++) {
+            for (int j = 0; j < tdarr.length; j++) {
+                System.out.print(tdarr[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
     
+    public static void main(String[] args) {
+        KeyandLock kl = new KeyandLock();
+        int[][] key = { {0,0,0},
+                        {1,0,0},
+                        {0,1,1}};
+        int[][] turnedKey = new int[key.length][key.length];
+        turnedKey = kl.rightTurnKey(key);
+        kl.printTwoDimensionalArray(turnedKey);
+        
+    }
 }
