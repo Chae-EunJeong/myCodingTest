ef872f947c067cdcfc135064908d42277a9fd273
Chae-EunJeong <chaeun.jjj@gmail.com>
2021-02-10 13:10:14 +0900

    11727

--- BOJ/DP/TwoXnTiling.java
+++ BOJ/DP/TwoXnTiling.java
@@ -29,5 +29,4 @@ public class TwoXnTiling {
         }
         System.out.println(dp[n]);
     }
-
 }
--- BOJ/DP/TwoXnTilingTwo.java
+++ BOJ/DP/TwoXnTilingTwo.java
@@ -5,7 +5,7 @@
     - restriction : 1 <= N <= 1000
                     N은 자연수
     - output : 2xn 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지
-- 점화식 : 
+- 점화식 : dp[n] = dp[n-1] + dp[n-2] * 2
 */
 
 package BOJ.DP;
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TwoXnTilingTwo {
+    //static int[] dp = new int[1001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -23,8 +24,21 @@ public class TwoXnTilingTwo {
         dp[0] = dp[1] = 1;
 
         for(int i = 2; i <= n; i++) {
-            dp[i] = (2 * (dp[i-1] + dp[i-2]) - 1)%10007;
+            dp[i] = (dp[i-1] + dp[i-2] * 2)%10007;
         }
         System.out.println(dp[n]);
+        //System.out.println(tiling(n));
     }
+    
+    //top-down
+    /*static int tiling(int n) {
+        if(n <= 2) {
+            return dp[n];
+        }
+        if(dp[n] != 0) {
+            return dp[n];
+        }
+        dp[n] = tiling(n-1) + tiling(n-2) * 2;
+        return dp[n];
+    }*/
 }
