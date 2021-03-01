8d7ae06e81acba8c2e66ddc3f501bb02376ea188
HyunJoon Jeong <chaeun.jjj@gmail.com>
2021-03-01 19:33:45 +0900

    오르막 수

--- BOJ/DP/AscendingNumber.java
+++ BOJ/DP/AscendingNumber.java
@@ -17,4 +17,21 @@ public class AscendingNumber {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        long dp[][] = new long[n+1][10];
+        int sum = 0;
+        
+        for(int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j < 10; j++) {
+                dp[i][j] += dp[i-1][j];
+            }
+        }
+
+        for(int i = 0; i < 10; i++) {
+            sum += dp[n][i];
+        }
+    }
 }
