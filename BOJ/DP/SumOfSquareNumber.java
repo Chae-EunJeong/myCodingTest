0f1835064030317371c6756eb339f26089ed7d99
Chae-EunJeong <chaeun.jjj@gmail.com>
2021-04-14 23:14:02 +0900

    1699

--- BOJ/DP/SumOfSquareNumber.java
+++ BOJ/DP/SumOfSquareNumber.java
@@ -11,8 +11,31 @@
     - 
 */
 
-package BOJ.DP;
+//package BOJ.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class SumOfSquareNumber {
-    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int dp[] = new int[n+1];
+        int min = 0; 
+
+        for(int i = 0; i <= n; i++) {
+            dp[i] = i;
+        }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; i - (j * j) >= 0; j++) {
+                if(dp[i - (j * j)] + 1 < dp[i]) {
+                    dp[i] = dp[i - (j * j)] + 1;
+                }
+            }
+        }
+        System.out.println(dp[n]);
+
+        br.close();
+    }
 }
